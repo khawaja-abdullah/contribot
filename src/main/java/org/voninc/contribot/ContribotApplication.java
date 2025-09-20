da8ff45f70b94d0339b7619d57e94c924cdaff7f
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2025-09-20 18:17:36 +0500

    init from starter

---
+++ src/main/java/org/voninc/contribot/ContribotApplication.java
@@ -0,0 +1,13 @@
+package org.voninc.contribot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContribotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ContribotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/voninc/contribot/ContribotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.voninc.contribot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContribotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
