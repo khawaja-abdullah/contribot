b57d631f8629cf33c64a896c72a0cd766dbbb70b
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2025-11-07 16:17:53 +0500

    remove unused import

--- src/main/java/org/voninc/contribot/dao/JobExecutionFSRepository.java
+++ src/main/java/org/voninc/contribot/dao/JobExecutionFSRepository.java
@@ -29,7 +29,6 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 @Repository
 public class JobExecutionFSRepository implements IJobExecutionRepository {
